163d322088b23c93bb46d4f285ddf43388b87be4
Zak Thomas <up810144@myport.ac.uk>
2020-03-19 12:01:09 +0000

    updating script

--- MLstuff/MLtest.py
+++ MLstuff/MLtest.py
@@ -3,31 +3,23 @@ import xgboost as xgb
 from astropy.io import fits
 import pandas as pd
 from sklearn.metrics import mean_squared_error
-import numpy as np
+import numpy as npx
 import pylab
-input_file='/home/zak/Documents/Project4Yr/MaStarTrainingTest.fits'
+import numpy as np
+#import spectral_resampling as sp
+from scipy.signal import find_peaks
+import scipy as sp
+
+
+input_file='/home/zak/Documents/Project4Yr/MLstuff/MaStarTrainingTest.fits'
 hdu = fits.open(input_file)
 wavelength = hdu[1].data['wave']
 flux = hdu[1].data['features']
 metal = hdu[1].data['Z']
-hdu.info()
+s
 fluxTrain,fluxTest,metalTrain,metalTest = train_test_split(flux,metal,random_state=1)
-print(np.shape(metalTrain))
-print(np.shape(fluxTrain))
-print(np.shape(metalTest))
-print(np.shape(fluxTest))
-D_train = xgb.DMatrix(fluxTrain, label=metalTrain)
-D_test = xgb.DMatrix(fluxTest, label=metalTest)
-'''
-param = {
-    'eta': 0.3,
-    'max_depth': 3,
-    'objective': 'multi:softmax',
-    'num_class': 3}
 
-steps = 20  # The number of training iterations
-model = xgb.train(param, D_train, steps)
-'''
+
 
 xg_reg = xgb.XGBRegressor(objective ='reg:linear', colsample_bytree = 0.3, learning_rate = 0.1, max_depth = 5, alpha = 10, n_estimators = 10)
 
@@ -37,27 +29,41 @@ print(np.max(xg_reg.feature_importances_))
 
 rmse = np.sqrt(mean_squared_error(metalTest, preds))
 print("RMSE: %f" % (rmse))
+'''
+#importing trial data
+test_file = '/home/zak/Documents/Project4Yr//firefly_release_0_1_1/example_data/spectra/0266/spec-0266-51630-0623.fits'
+hdu = fits.open(test_file)
+t = hdu['COADD'].data
 
-test_file = '/home/zak/Documents/Project4Yr/firefly_release_0_1_1/example_data/spectra/0266/spec-0266-51630-0623.fits'
+mask = (t['loglam'] > np.log10(3620)) & (t['loglam'] < np.log10(10355)) # masking areas the models dont conver
+redshift = hdu[2].data['Z']
+trialflux = t['flux'][mask]
+print(trialflux)
+loglam_gal = t['loglam'][mask]
+galaxy = trialflux/np.median(trialflux)
+trialwave = 10**loglam_gal
+restframe_wavelength = trialwave/(1+redshift)
+# making the flux arrays the same size
+newflux = sp.spectres(wavelength[1],restframe_wavelength,galaxy)
+'''
+test_file = '/home/zak/Documents/Project4Yr/MLstuff/manga-8078-12701-LOGCUBE.fits'
+hdu = fits.open(test_file)
+mask = (trialwave > 3620) & (trialwave < 10355) # masking areas the models dont conver
+
+trialflux = hdu[1].data[:,37,37][mask]
+galaxy = trialflux/np.median(trialflux)
+trialwave = hdu[6].data[mask]
+interp = sp.interpolate.UnivariateSpline(trialwave,galaxy, s = 70)
+sub_flux = galaxy - interp(trialwave)
+
 #convert to restframe
 #try scikit learn decision trees
 #test on manga data
 #actually normalise continuum
-hdu = fits.open(test_file)
-t = hdu['COADD'].data
-z = hdu[2].data['Z']
-#mask = (t['loglam'] > np.log10(3621)) & (t['loglam'] < np.log10(10352))
-fluxy = t['flux']
-loglam_gal = t['loglam']
-galaxy = fluxy/np.median(fluxy)
-print(np.shape(galaxy))
-gal = galaxy
-print(galaxy)
-print(gal)
-stack =np.stack((galaxy,gal))
-print((stack))
-print(stack.dtype)
-pred2=xg_reg.predict(stack.T)
-dtest = xgb.DMatrix(,galaxy)
-print(np.shape(fluxTest))
-pr
+
+
+gal = sub_flux
+
+stack =np.stack((sub_flux,gal))
+pred2=xg_reg.predict(stack)
+print(pred2)
